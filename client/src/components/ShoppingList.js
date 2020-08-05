30649df67beb0f5477f0301363a9a9e16ea5f4cc
Serj1c <sergej.a.isaev@gmail.com>
2020-08-05 07:10:20 +0300

    basic mern-stuff done and worked

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
+
+class ShoppingList extends Component {
+
+    componentDidMount() {
+        this.props.getItems();
+    }
+
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id);
+    }
+    
+    render() {
+        const { items } = this.props.item;
+        return(
+            <Container>
+                
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({ _id, name }) => (
+                            <CSSTransition key={_id} classNames="fade" timeout={500}>
+                                <ListGroupItem>
+                                    <Button
+                                        className="remove-btn"
+                                        size="sm"
+                                        color="danger"
+                                        onClick={this.onDeleteClick.bind(this, _id)}
+                                    >&times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        );
+    }
+};
+
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    item: state.item
+});
+
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
