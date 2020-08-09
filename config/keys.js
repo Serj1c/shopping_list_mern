2bcc0adbe663da68a6b9bdc597c2db0201dad677
Serj1c <sergej.a.isaev@gmail.com>
2020-08-09 21:00:35 +0300

    auth and jwt added to backend

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://serjic:spartak1@cluster0.9bgx2.mongodb.net/<dbname>?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
-
-const items = require('./routes/api/items');
+const config = require('config');
 
 const app = express();
 
 // Body-parser middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to MongoDB
-mongoose.connect(db)
+mongoose.connect(db, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
     .then(() => console.log('MongoDB connected!'))
     .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
